83dd4f0c135dafda767725746de5f3ab9b1b6dcb
DuyNguyen994 <duynguyen06031994@gmail.com>
2023-10-15 10:15:32 +0700

    Nguyen Tran Thanh Duy

---
+++ BackEnd/src/main/java/com/cybersoft/cozastore/entity/BlogEntity.java
@@ -0,0 +1,31 @@
+ package com.cybersoft.cozastore.entity;
+ import lombok.Data;
+
+ import javax.persistence.*;
+ import java.util.Date;
+
+
+@Data
+@Entity(name = "blog")
+public class BlogEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  
+  @Column(name = "title")
+  private String title;
+  
+  @Column(name = "image")
+  private String image;
+  
+  @Column(name = "content")
+  private String content;
+  
+  @Column(name = "idUser")
+  private int idUser;
+  
+  @Column(name = "createDate")
+  private Date createDate;
+
+}
