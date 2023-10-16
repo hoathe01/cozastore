2f68a48af51c536bf647d1350e3d2cf963e1c2bd
unknown <nguyenchauhuutan@gmail.com>
2023-10-16 10:23:06 +0700

    Nguyen Chau Huu Tan

--- BackEnd/src/main/java/com/cybersoft/cozastore/entity/BlogTagEntity.java
+++ BackEnd/src/main/java/com/cybersoft/cozastore/entity/BlogTagEntity.java
@@ -1,14 +1,16 @@
- package com.none.entity;
- import jakarta.persistence.*;
+ package com.cybersoft.cozastore.entity;
  import lombok.Data;
+
+ import javax.persistence.Column;
+ import javax.persistence.Entity;
+ import javax.persistence.Id;
  import java.util.Date;
 
 
 @Data
 @Entity(name = "blogtag" )
 public class BlogTagEntity {
-
-  
+    @Id
   @Column(name = "idBlog")
   private int idBlog;
   
