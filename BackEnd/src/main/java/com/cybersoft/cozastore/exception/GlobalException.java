3373ca03a5e0ac915b1c11bf9b4bcc679c234564
maliketh <hoathe8945@gmail.com>
2023-10-21 13:17:46 +0700

    Huynh The Hoa

--- BackEnd/src/main/java/com/cybersoft/cozastore/exception/GlobalException.java
+++ BackEnd/src/main/java/com/cybersoft/cozastore/exception/GlobalException.java
@@ -2,9 +2,13 @@ package com.cybersoft.cozastore.exception;
 
 import com.cybersoft.cozastore.payload.BaseResponse;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,15 +16,25 @@ import java.sql.SQLException;
 
 @ControllerAdvice
 public class GlobalException {
-    @ExceptionHandler(value = Exception.class)
+    @ExceptionHandler(value = {AuthenticationCredentialsNotFoundException.class, AccessDeniedException.class})
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<?> unauthorize(HttpServletRequest req, Exception e) throws Exception {
+        BaseResponse response = new BaseResponse();
+        response.setStatusCode(401);
+        response.setMessage(e.getLocalizedMessage());
+        response.setData(null);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(401));
+    }
+    @ExceptionHandler(value = {SQLException.class, RuntimeException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<?> defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
-        if (AnnotationUtils.findAnnotation
-                (e.getClass(), ResponseStatus.class) != null)
-            throw e;
+
         BaseResponse response = new BaseResponse();
         response.setStatusCode(500);
         response.setMessage(e.getLocalizedMessage());
         response.setData(null);
-       return ResponseEntity.ok(response);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(500));
     }
+
+
 }
